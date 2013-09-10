124e3da582b616de56d7a8c55ed5bfd0798de0e3
artur <mkrtchyan.artur@gmail.com>
2013-09-10 23:03:24 +0200

    initial project structure with dummy App class

---
+++ src/main/java/org/iban4j/App.java
@@ -0,0 +1,13 @@
+package org.iban4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
