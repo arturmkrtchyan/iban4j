c016b590f53a6dd126979ff8d291e5293c2be3d0
artur <mkrtchyan.artur@gmail.com>
2013-09-30 21:12:22 +0200

    added equals and hashcode for Iban

--- src/main/java/org/iban4j/Iban.java
+++ src/main/java/org/iban4j/Iban.java
@@ -7,4 +7,26 @@ public final class Iban {
     public Iban(String value) {
         this.value = value;
     }
+
+    private static Iban valueOf(String value) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Integer) {
+            return value.equals(((Iban)obj).toString());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
