19afa5199e45ebffbb51eede9e1b4cebb1d7438b
artur <mkrtchyan.artur@gmail.com>
2013-10-08 13:46:02 +0200

    Added check digit calculation in IbanUtil class

--- src/main/java/org/iban4j/IbanUtil.java
+++ src/main/java/org/iban4j/IbanUtil.java
@@ -1,4 +1,34 @@
 package org.iban4j;
 
 public class IbanUtil {
+
+    private static final long MOD = 97;
+    private static final long MAX = 999999999;
+
+    static CheckDigit calculateCheckDigit(String iban) {
+        int modResult = calculateMod(iban);
+        int checkDigitIntValue = (98 - modResult);
+        String checkDigit = Integer.toString(checkDigitIntValue);
+        return new CheckDigit(checkDigitIntValue > 9 ? checkDigit : "0" + checkDigit);
+    }
+
+    private static int calculateMod(String iban) {
+        String reformattedIban = iban.substring(4) + iban.substring(0, 4);
+        long total = 0;
+        for (int i = 0; i < reformattedIban.length(); i++) {
+            int numericValue = Character.getNumericValue(reformattedIban.charAt(i));
+            if (numericValue < 0 || numericValue > 35) {
+                throw new IllegalArgumentException("Invalid Character[" + i + "] = '" + numericValue + "'");
+            }
+            total = (numericValue > 9 ? total * 100 : total * 10) + numericValue;
+
+            if (total > MAX) {
+                total = (total % MOD);
+            }
+
+        }
+        return (int)(total % MOD);
+    }
+
+
 }
