95cba02297e701029787ad8b13b8b24ee9ab5881
artur <mkrtchyan.artur@gmail.com>
2013-11-22 22:04:55 +0100

    Added package-info files

---
+++ src/main/java/org/iban4j/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ *
+ * Root package for iban4j implementation.
+ * Provides a builder class and utilities to generate IBAN and calculate check digit.
+ * Includes exceptions for IBAN generation.
+ *
+ */
+package org.iban4j;
---
+++ src/main/java/org/iban4j/support/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ *
+ * Support classes for iban4j implementation.
+ *
+ */
+package org.iban4j.support;
