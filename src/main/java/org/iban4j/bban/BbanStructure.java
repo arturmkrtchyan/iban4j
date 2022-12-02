f746501966b2fe8605b6e9378199553d1f7928cd
dvg2207x <lx14578@innovaccion.mx>
2022-12-02 14:57:52 -0400

    Adding Gabon BbanStructure
    Added correct BbanStructure for Gabon and included Test case

--- src/main/java/org/iban4j/bban/BbanStructure.java
+++ src/main/java/org/iban4j/bban/BbanStructure.java
@@ -204,6 +204,12 @@ public class BbanStructure {
                         BbanStructureEntry.branchCode(5, 'n'),
                         BbanStructureEntry.accountNumber(13, 'c')));
 
+        structures.put(CountryCode.GA,
+                new BbanStructure(
+                        BbanStructureEntry.bankCode(5, 'n'),
+                        BbanStructureEntry.branchCode(5, 'n'),
+                        BbanStructureEntry.accountNumber(13, 'c')));
+
         structures.put(CountryCode.GE,
                 new BbanStructure(
                         BbanStructureEntry.bankCode(2, 'a'),
