83b545ef8b0289074e58b08c7ea1214cc45d83b5
Kayvan Tehrani <k1.tehrani@gmail.com>
2023-08-26 00:19:29 +0330

    Issue 101 (#113)
    * adding two new methods
    
    * adding test for the isValid false results
    
    * adding missing tests
    
    * adding valid tests for isValid

--- src/test/java/org/iban4j/IbanUtilTest.java
+++ src/test/java/org/iban4j/IbanUtilTest.java
@@ -16,38 +16,55 @@
 package org.iban4j;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @DisplayName("IbanUtilTest")
 public class IbanUtilTest {
     
     @DisplayName("ibanCountrySupportCheckWithNullShouldReturnFalse")
+    @Test
     public void ibanCountrySupportCheckWithNullShouldReturnFalse() {
         assertThat(IbanUtil.isSupportedCountry(null), is(equalTo(false)));
     }
 
     @DisplayName("ibanCountrySupportCheckWithSupportedCountryShouldReturnTrue")
+    @Test
     public void ibanCountrySupportCheckWithSupportedCountryShouldReturnTrue() {
         assertThat(IbanUtil.isSupportedCountry(CountryCode.BE), is(equalTo(true)));
     }
 
     @DisplayName("ibanCountrySupportCheckWithUnsupportedCountryShouldReturnFalse")
+    @Test
         public void ibanCountrySupportCheckWithUnsupportedCountryShouldReturnFalse() {
             assertThat(IbanUtil.isSupportedCountry(CountryCode.AM), is(equalTo(false)));
         }
 
     @DisplayName("unformattedIbanValidationWithNoneFormattingShouldNotThrowException")
+    @Test
         public void unformattedIbanValidationWithNoneFormattingShouldNotThrowException() {
             IbanUtil.validate("AT611904300234573201", IbanFormat.None);
         }
+    @DisplayName("unformattedIbanValidationWithNoneFormattingShouldNotThrowException")
+    @Test
+        public void unformattedIbanIsValidWithNoneFormattingShouldNotThrowException() {
+            assertTrue(IbanUtil.isValid("AT611904300234573201", IbanFormat.None));
+        }
 
     @DisplayName("formattedIbanValidationWithDefaultFormattingShouldNotThrowException")
+    @Test
         public void formattedIbanValidationWithDefaultFormattingShouldNotThrowException() {
             IbanUtil.validate("AT61 1904 3002 3457 3201", IbanFormat.Default);
+        }
+    @DisplayName("formattedIbanValidationWithDefaultFormattingShouldNotThrowException")
+    @Test
+        public void formattedIbanIsValidWithDefaultFormattingShouldNotThrowException() {
+            assertTrue(IbanUtil.isValid("AT61 1904 3002 3457 3201", IbanFormat.Default));
         }
     }
 
