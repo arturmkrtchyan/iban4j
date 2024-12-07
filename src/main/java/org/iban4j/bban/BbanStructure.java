69945e09694f84e1db00c2d294a8f93c7a7b0366
PabloLec <pablo.lecolinet@gmail.com>
2024-12-07 13:40:47 +0100

    Fix formatting

--- src/main/java/org/iban4j/bban/BbanStructure.java
+++ src/main/java/org/iban4j/bban/BbanStructure.java
@@ -724,8 +724,8 @@ public class BbanStructure {
                                                      final String entryValue) {
     switch (entry.getCharacterType()) {
       case a:
-        for(char ch: entryValue.toCharArray()) {
-          if(!Character.isUpperCase(ch)) {
+        for (char ch: entryValue.toCharArray()) {
+          if (!Character.isUpperCase(ch)) {
             throw new IbanFormatException(BBAN_ONLY_UPPER_CASE_LETTERS,
                     entry.getEntryType(), entryValue, ch,
                     String.format(ASSERT_UPPER_LETTERS, entryValue));
@@ -733,8 +733,8 @@ public class BbanStructure {
         }
         break;
       case c:
-        for(char ch: entryValue.toCharArray()) {
-          if(!Character.isLetterOrDigit(ch)) {
+        for (char ch: entryValue.toCharArray()) {
+          if (!Character.isLetterOrDigit(ch)) {
             throw new IbanFormatException(BBAN_ONLY_DIGITS_OR_LETTERS,
                     entry.getEntryType(), entryValue, ch,
                     String.format(ASSERT_DIGITS_AND_LETTERS, entryValue));
@@ -742,8 +742,8 @@ public class BbanStructure {
         }
         break;
       case n:
-        for(char ch: entryValue.toCharArray()) {
-          if(!Character.isDigit(ch)) {
+        for (char ch: entryValue.toCharArray()) {
+          if (!Character.isDigit(ch)) {
             throw new IbanFormatException(BBAN_ONLY_DIGITS,
                     entry.getEntryType(), entryValue, ch,
                     String.format(ASSERT_DIGITS, entryValue));
