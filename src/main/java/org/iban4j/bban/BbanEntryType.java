cf064b9c819ab5dbeee535e194198e823bbe2796
hajk1 <k1.tehrani@gmail.com>
2025-04-30 18:36:33 +0400

    apply google code style

--- src/main/java/org/iban4j/bban/BbanEntryType.java
+++ src/main/java/org/iban4j/bban/BbanEntryType.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.iban4j.bban;
 
 /** Basic Bank Account Number Entry Types. */
--- src/main/java/org/iban4j/bban/BbanStructureEntry.java
+++ src/main/java/org/iban4j/bban/BbanStructureEntry.java
@@ -19,46 +19,43 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-/**
- * Bban Structure Entry representation.
- */
+/** Bban Structure Entry representation. */
 public class BbanStructureEntry {
 
-    private final BbanEntryType entryType;
-    private final EntryCharacterType characterType;
-    private final int length;
+  private static final Map<EntryCharacterType, char[]> charByCharacterType;
 
-    private static final Map<EntryCharacterType, char[]> charByCharacterType;
-
-    static {
-        charByCharacterType = new HashMap<>();
-        StringBuilder charTypeN = new StringBuilder();
-        for (char ch = '0'; ch <= '9'; ch++) {
-            charTypeN.append(ch);
-        }
-        charByCharacterType.put(EntryCharacterType.n, charTypeN.toString().toCharArray());
-
-        StringBuilder charTypeA = new StringBuilder();
-        for (char ch = 'A'; ch <= 'Z'; ++ch) {
-            charTypeA.append(ch);
-        }
-        charByCharacterType.put(EntryCharacterType.a, charTypeA.toString().toCharArray());
-
-        charByCharacterType.put(EntryCharacterType.c, (charTypeN.toString() + charTypeA.toString()).toCharArray());
+  static {
+    charByCharacterType = new HashMap<>();
+    StringBuilder charTypeN = new StringBuilder();
+    for (char ch = '0'; ch <= '9'; ch++) {
+      charTypeN.append(ch);
     }
+    charByCharacterType.put(EntryCharacterType.n, charTypeN.toString().toCharArray());
 
-    private BbanStructureEntry(final BbanEntryType entryType,
-                       final EntryCharacterType characterType,
-                       final int length) {
-        this.entryType = entryType;
-        this.characterType = characterType;
-        this.length = length;
+    StringBuilder charTypeA = new StringBuilder();
+    for (char ch = 'A'; ch <= 'Z'; ++ch) {
+      charTypeA.append(ch);
     }
+    charByCharacterType.put(EntryCharacterType.a, charTypeA.toString().toCharArray());
 
-    public static BbanStructureEntry bankCode(final int length, final char characterType) {
-        return new BbanStructureEntry(BbanEntryType.bank_code,
-                EntryCharacterType.valueOf(String.valueOf(characterType)), length);
-    }
+    charByCharacterType.put(EntryCharacterType.c, (charTypeN.toString() + charTypeA).toCharArray());
+  }
+
+  private final BbanEntryType entryType;
+  private final EntryCharacterType characterType;
+  private final int length;
+
+  private BbanStructureEntry(
+      final BbanEntryType entryType, final EntryCharacterType characterType, final int length) {
+    this.entryType = entryType;
+    this.characterType = characterType;
+    this.length = length;
+  }
+
+  public static BbanStructureEntry bankCode(final int length, final char characterType) {
+    return new BbanStructureEntry(
+        BbanEntryType.bank_code, EntryCharacterType.valueOf(String.valueOf(characterType)), length);
+  }
 
   public static BbanStructureEntry bankCodeExt(final int length, final char characterType) {
     return new BbanStructureEntry(
@@ -67,92 +64,106 @@ public class BbanStructureEntry {
         length);
   }
 
-    public static BbanStructureEntry branchCode(final int length, final char characterType) {
-        return new BbanStructureEntry(BbanEntryType.branch_code,
-                EntryCharacterType.valueOf(String.valueOf(characterType)), length);
+  public static BbanStructureEntry branchCode(final int length, final char characterType) {
+    return new BbanStructureEntry(
+        BbanEntryType.branch_code,
+        EntryCharacterType.valueOf(String.valueOf(characterType)),
+        length);
+  }
+
+  public static BbanStructureEntry accountNumber(final int length, final char characterType) {
+    return new BbanStructureEntry(
+        BbanEntryType.account_number,
+        EntryCharacterType.valueOf(String.valueOf(characterType)),
+        length);
+  }
+
+  public static BbanStructureEntry nationalCheckDigit(final int length, final char characterType) {
+    return new BbanStructureEntry(
+        BbanEntryType.national_check_digit,
+        EntryCharacterType.valueOf(String.valueOf(characterType)),
+        length);
+  }
+
+  public static BbanStructureEntry accountType(final int length, final char characterType) {
+    return new BbanStructureEntry(
+        BbanEntryType.account_type,
+        EntryCharacterType.valueOf(String.valueOf(characterType)),
+        length);
+  }
+
+  public static BbanStructureEntry ownerAccountNumber(final int length, final char characterType) {
+    return new BbanStructureEntry(
+        BbanEntryType.owner_account_number,
+        EntryCharacterType.valueOf(String.valueOf(characterType)),
+        length);
+  }
+
+  public static BbanStructureEntry identificationNumber(
+      final int length, final char characterType) {
+    return new BbanStructureEntry(
+        BbanEntryType.identification_number,
+        EntryCharacterType.valueOf(String.valueOf(characterType)),
+        length);
+  }
+
+  public BbanEntryType getEntryType() {
+    return entryType;
+  }
+
+  public EntryCharacterType getCharacterType() {
+    return characterType;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public String getRandom() {
+    return getRandom(new Random());
+  }
+
+  public String getRandom(Random random) {
+
+    // Create a new seeded Random, so it doesn't matter how this Random is used, it won't affect
+    // subsequent usages
+    // of the original Random. (which can impact seeded behaviour when many IBANs are generated or
+    // the number of
+    // IBAN entries change).
+    random = new Random(random.nextInt());
+
+    StringBuilder s = new StringBuilder();
+    char[] charChoices = charByCharacterType.get(characterType);
+    if (charChoices == null) {
+      throw new RuntimeException(
+          String.format(
+              "programmer has not implemented choices for character type %s",
+              characterType.name()));
     }
-
-    public static BbanStructureEntry accountNumber(final int length, final char characterType) {
-        return new BbanStructureEntry(BbanEntryType.account_number,
-                EntryCharacterType.valueOf(String.valueOf(characterType)), length);
+    for (int i = 0; i < getLength(); i++) {
+      s.append(charChoices[random.nextInt(charChoices.length)]);
     }
+    return s.toString();
+  }
 
-    public static BbanStructureEntry nationalCheckDigit(final int length, final char characterType) {
-        return new BbanStructureEntry(BbanEntryType.national_check_digit,
-                EntryCharacterType.valueOf(String.valueOf(characterType)), length);
-    }
+  @Override
+  public String toString() {
+    return "BbanStructureEntry{"
+        + "entryType="
+        + entryType
+        + ", characterType="
+        + characterType
+        + ", length="
+        + length
+        + '}';
+  }
 
-    public static BbanStructureEntry accountType(final int length, final char characterType) {
-        return new BbanStructureEntry(BbanEntryType.account_type,
-                EntryCharacterType.valueOf(String.valueOf(characterType)), length);
-    }
-
-    public static BbanStructureEntry ownerAccountNumber(final int length, final char characterType) {
-        return new BbanStructureEntry(BbanEntryType.owner_account_number,
-                EntryCharacterType.valueOf(String.valueOf(characterType)), length);
-    }
-
-    public static BbanStructureEntry identificationNumber(final int length, final char characterType) {
-        return new BbanStructureEntry(BbanEntryType.identification_number,
-                EntryCharacterType.valueOf(String.valueOf(characterType)), length);
-    }
-
-    public BbanEntryType getEntryType() {
-        return entryType;
-    }
-
-    public EntryCharacterType getCharacterType() {
-        return characterType;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public enum EntryCharacterType {
-        /**
-         * Numerical digits (0-9 only)
-         */
-        n,
-        /**
-         * Alphabetical characters (A-Z only)
-         */
-        a,
-        /**
-         * Combined alphabetical (uppercase) and numeric characters (A-Z and 0-9)
-         */
-        c
-    }
-
-    public String getRandom() {
-        return getRandom(new Random());
-    }
-
-    public String getRandom(Random random) {
-
-        // Create a new seeded Random, so it doesn't matter how this Random is used, it won't affect subsequent usages
-        // of the original Random. (which can impact seeded behaviour when many IBANs are generated or the number of
-        // IBAN entries change).
-        random = new Random(random.nextInt());
-
-        StringBuilder s = new StringBuilder();
-        char[] charChoices = charByCharacterType.get(characterType);
-        if (charChoices == null) {
-            throw new RuntimeException(String.format("programmer has not implemented choices for character type %s",
-                    characterType.name()));
-        }
-        for (int i = 0; i < getLength(); i++) {
-            s.append(charChoices[random.nextInt(charChoices.length)]);
-        }
-        return s.toString();
-    }
-
-    @Override
-    public String toString() {
-        return "BbanStructureEntry{" +
-            "entryType=" + entryType +
-            ", characterType=" + characterType +
-            ", length=" + length +
-            '}';
-    }
+  public enum EntryCharacterType {
+    /** Numerical digits (0-9 only) */
+    n,
+    /** Alphabetical characters (A-Z only) */
+    a,
+    /** Combined alphabetical (uppercase) and numeric characters (A-Z and 0-9) */
+    c
+  }
 }
